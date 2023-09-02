c9594ef0ccec30906ad5df097f831abf268dd49e
JeongPil Cheon <cheonjp87@gmail.com>
2023-09-03 01:13:28 +0900

    made select and options for order page

--- src/assets/components/select/Select.jsx
+++ src/assets/components/select/Select.jsx
@@ -1,18 +1,22 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { BsChevronDown } from "react-icons/bs"
 import "./select.scss"
 
-const tests = ["test", "test2", "hello World"]
-function Select() {
+// function Select({textTitle,optionArray,refer,targetFunction}) {
+function Select(props) {
+    const {textTitle,optionArray,refer}=props
     const [openSelect, setOpenSelect] = useState(false)
     const [firstValue, setFirstValue] = useState("")
 
-    const targetOption = useRef()
 
     const activeSelect = (e) => {
         setOpenSelect(!openSelect)
     }
 
+    useEffect(()=>{
+
+    },[firstValue])
+    
     const optionEvent = (e) => {
         const parent = e.target.parentElement.parentElement
         const options = parent.querySelectorAll(".optionBox")
@@ -22,21 +26,21 @@ function Select() {
         setFirstValue(e.target.textContent)
         
         e.currentTarget.classList.add("selected")
-
     }
     window.onclick = (e) => {
         e.target.closest(".select") || setOpenSelect(false)
 
     }
+    
     return (
-        <div className='select' onClick={activeSelect}>
+        <div className='select' >
             <div className="selectContainer">
-                <div className="title">Sort by</div>
-                <div className="select"><span>{firstValue === "" ? tests[0] : firstValue}</span></div>
+                <div className="title">{textTitle}</div>
+                <div className="select"><span value ={firstValue} ref={refer}>{firstValue === "" ? optionArray[0] : firstValue}</span></div>
             </div>
             <div className={openSelect ? "optionContainer active" : "optionContainer"}>
                 {
-                    tests.map((option,index) => {
+                    optionArray.map((option,index) => {
                         return (
                             <div className={index === 0 ? "optionBox selected" : "optionBox"} onClick={optionEvent}>
                                 <div className="option" value={option}>{option}</div>
--- src/assets/pages/order/Order.jsx
+++ src/assets/pages/order/Order.jsx
@@ -1,39 +1,190 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import "./order.scss"
 import instance from '../../../axios'
-import Select from '../../components/select/Select'
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
 
 function Order() {
   const [items, setItems] = useState([])
-  const [sizes, setSizes]=useState([12,14,16])
+  const [sortReady, setSortReady] = useState(false)
+  const [sizes, setSizes] = useState([12, 14, 16])
+
+  const [menu, setMenu] = useState("All")
+  const [sort, setSort] = useState("A-Z")
+  const [allMenu, setAllMenu] = useState([])
+  const [sortedItems, setSortedItems] = useState([])
+
+  const menuHandleChange = (e) => {
+    setMenu(e.target.value)
+  }
+  const sortHandleChange = (e) => {
+    setSort(e.target.value)
+  }
+
+
+
+  const viewByMenu = () => {
+    if (menu === "All") {
+      setItems(allMenu)
+    }
+    if (menu === "Appetizer") {
+      const sort = allMenu.filter(item => item.cat === "Appetizer")
+      setItems(sort)
+    }
+    if (menu === "Pizza") {
+      const sort = allMenu.filter(item => item.cat === "Pizza")
+      setItems(sort)
+
+    }
+    if (menu === "Combo") {
+      const sort = allMenu.filter(item => item.cat === "Combo")
+      setItems(sort)
+
+    }
+    if (menu === "Drink") {
+      const sort = allMenu.filter(item => item.cat === "Drink")
+      setItems(sort)
+
+    }
+    if (menu === "Pasta") {
+      const sort = allMenu.filter(item => item.cat === "Pasta")
+      setItems(sort)
+
+    }
+  }
+
+  useEffect(() => {
+
+    viewByMenu()
+  }, [menu, sort,allMenu])
+
+  useEffect(()=>{
+    sortBy()
+  },[items])
+
+  const sortBy = () => {
+    if (sort === "Price high to low") {
+      const sortItem = items.sort((a, b) => {
+        if (typeof (a.price) === "object") {
+          a.price = a.price[0]
+        }
+        if (typeof (b.price) === "object") {
+          b.price = b.price[0]
+        }
+        return Number(b.price) - Number(a.price)
+      })
+      setSortedItems(sortItem)
+    }
+    if (sort === "Price low to high") {
+      const sortItem = items.sort((a, b) => {
+        if (typeof (a.price) === "object") {
+          a.price = a.price[0]
+        }
+        if (typeof (b.price) === "object") {
+          b.price = b.price[0]
+        }
+        return Number(a.price) - Number(b.price)
+      })
+      setSortedItems(sortItem)
+    }
+    if (sort === "A-Z") {
+      const sortItem = items.sort((a, b) => {
+        if (a.name > b.name) {
+          return 1
+        }
+        if (b.name > a.name) {
+          return -1
+        }
+        return 0
+      })
+      setSortedItems(sortItem)
+    }
+    if (sort === "Z-A") {
+      const sortItem = items.sort((a, b) => {
+        if (a.name > b.name) {
+          return -1
+        }
+        if (b.name > a.name) {
+          return 1
+        }
+        return 0
+      })
+      setSortedItems(sortItem)
+    }
+  }
+
+
+
+
   const getAllItems = async () => {
     const res = await instance.get("/api/menu/get_all")
+    setAllMenu(res.data)
     setItems(res.data)
+
   }
   useEffect(() => {
     getAllItems()
+    setSortReady(true)
   }, [])
+
 
   return (
     <div className='order'>
       <section>
         <h1>Menu</h1>
-        <div className="selectBox">
-          <Select/>
+        <div className="selectBoxContainer">
+          <FormControl sx={{ m: 1, width: 130 }} size="small">
+            <InputLabel id="demo-select-small-label">Menus</InputLabel>
+            <Select
+              labelId="demo-select-small-label"
+              id="demo-select-small"
+              value={menu}
+              label="Menus"
+              onChange={menuHandleChange}
+            >
+
+              <MenuItem value={"All"}>All</MenuItem>
+              <MenuItem value={"Appetizer"}>Appetizer</MenuItem>
+              <MenuItem value={"Pizza"}>Pizza</MenuItem>
+              <MenuItem value={"Pasta"}>Pasta</MenuItem>
+              <MenuItem value={"Combo"}>Combo</MenuItem>
+              <MenuItem value={"Drink"}>Drink</MenuItem>
+            </Select>
+          </FormControl>
+
+          <FormControl sx={{ m: 1, width: 200 }} size="small">
+            <InputLabel id="demo-select-small-label">Sort by</InputLabel>
+            <Select
+              labelId="demo-select-small-label"
+              id="demo-select-small"
+              value={sort}
+              label="Sort by"
+              onChange={sortHandleChange}
+            >
+
+              <MenuItem value={"A-Z"}>A-Z</MenuItem>
+              <MenuItem value={"Z-A"}>Z-A</MenuItem>
+              <MenuItem value={"Price high to low"}>Price high to low</MenuItem>
+              <MenuItem value={"Price low to high"}>Price low to high</MenuItem>
+
+            </Select>
+          </FormControl>
         </div>
         <div className="container">
-
-          {items.length > 0 && items.map((item,index) => {
+          {allMenu.length > 0 && sortedItems.map((item, index) => {
             return (
               <div className="item">
                 <img src={item.img} alt={item.name} />
                 <h4 className='itemName'>{item.name}</h4>
                 <p className='itemDesc'>{item.desc}</p>
                 <div className="priceBox">
-                  {typeof(item.price) === "object" && item.price.map((each,index)=>{
+                  {typeof (item.price) === "object" && item.price.map((each, index) => {
                     return <span className='itemPrice'>{sizes[index]}inch $ {each}</span>
                   })}
-                  {typeof(item.price) !=="object" && <span className='itemPrice'>$ {item.price}</span>}
+                  {typeof (item.price) !== "object" && <span className='itemPrice'>$ {item.price}</span>}
                 </div>
               </div>
             )
