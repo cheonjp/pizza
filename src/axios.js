02432c5b2de45e232784645b9661f2994ba58686
JeongPil Cheon <cheonjp87@gmail.com>
2023-09-14 09:06:39 +0900

    replace url to connenct to backend

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios"
-const instance = axios.create({baseURL:"http://localhost:5000"})
+// const instance = axios.create({baseURL:"http://localhost:5000"})
+const instance = axios.create({baseURL:"https://monster-pizza.onrender.com"})
 
 
 export default instance
